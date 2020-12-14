9812d6e3af63fce52938e010942b38e2a8509825
Bevan Swanson <bevan@thedatashed.co.uk>
2020-12-14 14:12:36 +0000

    stage 1: search for term x number of times in file

--- bevdb/__main__.py
+++ bevdb/__main__.py
@@ -1,11 +1,23 @@
-import sys
+import argparse
 
 def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+    file_path = "../../datasets/AD.txt"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("term", help="the term to search the file for", type=str)
+    parser.add_argument("times", help="the number of times to search the file", type=int)
+    term = parser.parse_args().term
+    times = parser.parse_args().times
+
+    print(f"Searching file for '{term}' {times} number of times")
+
+    search_results = []
+    with open(file_path) as file:
+        for num, line in enumerate(file, 1):
+            if len(search_results) < times and term in line:
+                search_results.append(num)
+
+    print(f"Your search term appears on lines: {search_results}")
 
 if __name__ == '__main__':
     main()
